ebaf6eb5a3a0ad88cef102ee4f6482e9d4cff564
David Anderson <david.ed.anderson@gmail.com>
2025-05-15 15:35:09 -0400

    move drift methods to drift_polys.jl

--- src/schub_polys.jl
+++ src/schub_polys.jl
@@ -2,10 +2,9 @@
 # David Anderson, April 2024
 
 
-export schub_poly, groth_poly, nschub, ngroth, dc2sd, ddx, pdx
+export schub_poly, groth_poly, nschub, ngroth, ddx, pdx
 
-# TO DO: clarify the logic and clean up tableau_components
-
+# TO DO: simplify calling methods for schub_poly
 #############
 
 """
@@ -298,26 +297,6 @@ end
 
 
 
-
-function schub_drifts( w::Vector{Int}, R::DoublePolyRing=xy_ring( max(length(w)-1,1), max(length(w)-1,1) )[1] )
-# compute schubert pol by drift class formula
-
-  fbpds = flat_bpds(w)
-
-  pol = R.ring(0)
-
-  for b in fbpds
-    b=markconfig(b)
-    pol = pol+dc2sd(b,R)
-  end
-
-  return pol
-
-end
-
-
-
-
 #@memoize 
 function schub_trans( w::Vector{Int}, R::DoublePolyRing=xy_ring( max(length(w)-1,1), max(length(w)-1,1) )[1] )
 # compute schubert pol by transition formula
@@ -535,101 +514,6 @@ function gp0( n::Int, R::DoublePolyRing=xy_ring( n-1, n-1 )[1] )
 
 end
 
-
-
-function dc2sd( dc::Drift, R::DoublePolyRing=xy_ring( size(dc.m)[1]-1, size(dc.m)[2]-1 )[1]  )
-# drift configuration to s-polynomial
-# must take marked configuration as input
-
-  local n=size(dc.m)[1]
-
-  for k=2*n:-1:2
-    for i=maximum([1,k-n]):minimum([n,k-1])
-      if isa( dc.m[i,k-i], Tuple ) && dc.m[i,k-i][2]
-        (dc1,dc2)=drift_split( dc, i, k-i )
-        return ( dc2sd( dc1, R ) + dc2sd( dc2, R ) )
-      end
-    end
-  end
-
-  sd = R.ring(1)
-
-  tcomps = tableau_components(dc)
-
-  for tt in tcomps
-    sd = sd*schur_poly( tt[1], tt[2], R; mu=tt[3], xoffset=tt[4][1], yoffset=tt[4][2], rowmin=true )
-  end
-
-  return sd
-
-end
-
-
-function tableau_components(dc::Drift)
-# return labelled tableaux for a drift configuration dc
-# must take marked configuration as input
-
-  local n=size(dc.m)[1]
-
-  if !isflat(dc)
-    return( tableau_components( nw_reset(dc) ) )
-  end
-
-  local lyds=Vector{Vector}([])
-
-  local corners=Vector{Tuple{Int,Int}}([])
-
-  for i=1:n
-    for j=1:n
-        if !( (i,j) in corners) && isa(dc.m[i,j],Tuple) && ((i,j)==(1,1) || (i>1 && j>1 && !isa( dc.m[i-1,j], Tuple) && !isa( dc.m[i,j-1],Tuple )  )) #find a new NW corner
-        push!(corners,(i,j))
-
-        local la=Vector{Int}([])
-        local mu=Vector{Int}([])
-        local rr=Vector{Int}([])
-
-        local s=0
-        while i+s<=n && isa( dc.m[i+s,j], Tuple )
-
-          local k=0
-          while j+k<=n && isa( dc.m[i+s,j+k], Tuple )  # find SE boxes
-            k +=1
-          end          
-          push!(la,k)
-
-
-          local kk=0
-          while j-kk-1>0 && isa( dc.m[i+s,j-kk-1], Tuple )  # find skew boxes
-            kk +=1
-          end
-
-          mu=mu+fill(kk,length(mu))
-          la=la+fill(kk,length(la))
-          push!(mu,0)
-
-          if s>0 && i+s>1 && j-kk>1 && ( dc.m[i+s-1,j-kk-1]==1 || dc.m[i+s-1,j-kk-1]==9 || dc.m[i+s-1,j-kk-1]==6  )
-            push!(corners,(i+s,j-kk) ) # record new corner
-          end
-          j=j-kk
-          s +=1
-        end
-
-        rr=Vector{Vector{Int}}([])
-        for el=1:length(la)
-          push!(rr, fill(0,mu[el]) )
-          for mm=mu[el]+1:la[el]
-            push!(rr[end], dc.m[i-1+el,j-1+mm][1]+el )
-          end
-        end
-
-        push!(lyds,[la,rr,mu,[i-1,j-1]])
-
-      end
-    end
-  end
-
-  return lyds
-end
 
 
 
